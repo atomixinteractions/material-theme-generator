fabb3d555808291d1e91fbbee82d9e5fbe21bd3f
Pavel Belik <razvanlomov@gmail.com>
2017-01-27 18:38:39 +0200

    add tabs

--- app/styles/Theme.js
+++ app/styles/Theme.js
@@ -4,28 +4,7 @@ import getMuiTheme from 'material-ui/styles/getMuiTheme';
 
 const Theme = getMuiTheme({
   palette: {
-    primary1Color: "#111111",
-    primary2Color: "#111111",
-    pickerHeaderColor: "#111111",
-    alternateTextColor: "#d9c77e",
-    accent3Color: "#d9c77e",
-    accent1Color: "#111111",
-  },
-  tabs: {
-    textColor: "#000000",
-    backgroundColor: "transparent",
-  },
-  raisedButton: {
-    color: "#fff",
-  },
-  toggle: {
-    thumbOnColor: "#d5c581",
-    thumbOffColor: "#fff",
-  },
-  checkbox: {
-    checkedColor: "#d9c77e",
-
-  },
+  }
 });
 
 export default Theme;
