3f3ebafb162f33d27734c3685f3dd32e45aaa714
Razzwan <razvanlomov@gmail.com>
2017-01-28 11:29:03 +0200

    base example

--- app/pages/AllComponentsViewPage.js
+++ app/pages/AllComponentsViewPage.js
@@ -1,55 +1,137 @@
-import React, { Component } from 'react';
-import TextField from 'material-ui/TextField'
-import { Tabs, Tab } from 'material-ui/Tabs'
-import Slider from 'material-ui/Slider'
+import React, { Component } from 'react'
+import { connect } from 'react-redux'
+import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
+import lightBaseTheme from '../themes/lightBaseTheme'
+import {
+  Checkbox,
+  DatePicker,
+  Dialog,
+  DropDownMenu,
+  FlatButton,
+  FloatingActionButton,
+  Drawer,
+  MenuItem,
+  Paper,
+  RadioButton,
+  RadioButtonGroup,
+  RaisedButton,
+  Snackbar,
+  Slider,
+  Tabs,
+  Tab,
+  TextField,
+  Toggle,
+} from 'material-ui'
 import injectTapEventPlugin from 'react-tap-event-plugin'
 injectTapEventPlugin()
 
-export default class AllComponentsViewPage extends Component {
-  render () {
-    return (
-      <div>
-        <TextField
-          floatingLabelText="Floating Label Text"
-          value="TextField without error"
-        />
-        <TextField
-          floatingLabelText="TextField wit error and empty value"
-          hintText="Hing text"
-          errorText="Error text"
-        />
+class AllComponentsViewPage extends Component {
+  state = {
+    theme: 'lightBaseTheme'
+  }
 
-        <Tabs>
-          <Tab label="Item One" >
-            <div>
-              <h2>Tab One</h2>
-              <p>
-                This is an example tab.
-              </p>
-              <p>
-                You can put any sort of HTML or react component in here. It even keeps the component state!
-              </p>
-              <Slider name="slider0" defaultValue={0.5} />
-            </div>
-          </Tab>
-          <Tab label="Item Two" >
-            <div>
-              <h2>Tab Two</h2>
-              <p>
-                This is another example tab.
-              </p>
-            </div>
-          </Tab>
-          <Tab label="onActive" data-route="/home">
-            <div>
-              <h2>Tab Three</h2>
-              <p>
-                This is a third example tab.
-              </p>
-            </div>
-          </Tab>
-        </Tabs>
-      </div>
+  constructor(props) {
+    super(props)
+    this.changeTheme = this.changeTheme.bind(this)
+    this.getMainColorByThemeName = this.getMainColorByThemeName.bind(this)
+  }
+
+  changeTheme(newThemeTitle) {
+    const self = this;
+    this.setState({
+      theme: newThemeTitle
+    }, () => {
+      self.props.changeTheme(newThemeTitle)
+    })
+  }
+
+  getMainColorByThemeName() {
+    switch (this.state.theme) {
+      case 'lightBaseTheme':
+        return '#fff'
+      case 'darkBaseTheme':
+        return '#303030'
+      default :
+        return '#fff'
+    }
+  }
+
+  render () {
+    console.log(this.props.theme)
+    const blockStyle = {
+      margin: '15px'
+    }
+    const elementStyle = {
+      margin: '10px'
+    }
+    return (
+      <MuiThemeProvider muiTheme={this.props.theme}>
+        <div style={{backgroundColor: this.getMainColorByThemeName()}}>
+          <Tabs value={this.state.theme} onChange={this.changeTheme}>
+            <Tab label="Light Base Theme" value={'lightBaseTheme'}/>
+            <Tab label="Dark Base Theme" value={'darkBaseTheme'}/>
+          </Tabs>
+
+          <Paper style={blockStyle}>
+            <Checkbox label="Test Checkbox"/>
+            <Checkbox disabled={true} label="Test Checkbox"/>
+          </Paper>
+
+          <Paper style={blockStyle} zDepth={1}>
+            <DatePicker/>
+          </Paper>
+
+          <Paper style={blockStyle} zDepth={2}>
+            <TextField style={elementStyle} floatingLabelText={'TextField'}/>
+            <TextField style={elementStyle} floatingLabelText={'TextField'} defaultValue={'Default Value'}/>
+            <TextField style={elementStyle} floatingLabelText={'TextField'} defaultValue={'Default Value'} errorText={'Error!'}/>
+            <DropDownMenu style={elementStyle}>
+              <MenuItem primaryText={'First'}/>
+              <MenuItem primaryText={'Second'}/>
+              <MenuItem primaryText={'Last'}/>
+            </DropDownMenu>
+          </Paper>
+
+          <Paper style={blockStyle} zDepth={3}>
+            <FlatButton style={elementStyle} label={'I am a FlatButton'}/>
+            <FlatButton style={elementStyle} primary={true} label={'I am a FlatButton'}/>
+            <FlatButton style={elementStyle} secondary={true} label={'I am a FlatButton'}/>
+          </Paper>
+
+          <Paper style={blockStyle} zDepth={4}>
+            <RaisedButton style={elementStyle} label={'I am a RaisedButton'} />
+            <RaisedButton style={elementStyle} primary={true} label={'I am a RaisedButton'} />
+            <RaisedButton style={elementStyle} secondary={true} label={'I am a RaisedButton'} />
+          </Paper>
+
+          <Paper style={blockStyle} zDepth={5}>
+            <FloatingActionButton style={elementStyle} label={'I am a RaisedButton'} />
+          </Paper>
+
+          <Paper style={blockStyle}>
+            <RadioButtonGroup style={elementStyle} name="ttt">
+              <RadioButton label="Name" value={1}/>
+              <RadioButton label={'Name2'} value={2}/>
+              <RadioButton label={'Name3'} value={3}/>
+            </RadioButtonGroup>
+          </Paper>
+
+          <Paper style={blockStyle}>
+            <Slider style={elementStyle} />
+            <Toggle label={}/>
+          </Paper>
+        </div>
+      </MuiThemeProvider>
     )
   }
 }
+
+export default connect(state => ({
+  theme: state.theme,
+}), dispatch => {
+  return {
+    changeTheme: (newThemeTitle) => {
+      dispatch({type: 'REPLACE_CURRENT_THEME', theme: require('../themes/' + newThemeTitle).default})
+    }
+  }
+})(AllComponentsViewPage)
--- app/root.js
+++ app/root.js
@@ -1,17 +1,32 @@
 import React from 'react'
-import { Router, Route } from 'react-router'
-import createBrowserHistory from 'history/lib/createBrowserHistory'
+import { createStore, combineReducers, applyMiddleware } from 'redux'
+import thunk from 'redux-thunk'
+import { Provider } from 'react-redux'
+import { Router, Route, IndexRoute, browserHistory } from 'react-router'
+import { syncHistoryWithStore, routerReducer } from 'react-router-redux'
 
-import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider'
-import Theme from './styles/Theme'
 import AllComponentsViewPage from './pages/AllComponentsViewPage'
 
+import theme from './reducers/theme'
+
+// Add the reducer to your store on the `routing` key
+const store = createStore(
+  combineReducers({
+    routing: routerReducer,
+    theme
+  }),
+  applyMiddleware(thunk)
+);
+
+// Create an enhanced history that syncs navigation events with the store
+const history = syncHistoryWithStore(browserHistory, store)
+
 const App = () => (
-  <MuiThemeProvider muiTheme={Theme}>
-    <Router history={createBrowserHistory()}>
+  <Provider store={store}>
+    <Router history={history}>
       <Route path="/" component={AllComponentsViewPage} />
     </Router>
-  </MuiThemeProvider>
+  </Provider>
 )
 
 export default App
