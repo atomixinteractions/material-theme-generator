b7690a6c2b7951879e0731ecb9542b93b238557e
Razzwan <razvanlomov@gmail.com>
2017-01-28 16:33:40 +0200

    remove coma

--- app/pages/AllComponentsViewPage.js
+++ app/pages/AllComponentsViewPage.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react'
 import { connect } from 'react-redux'
-import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
+import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider'
 import {
   Checkbox,
   DatePicker,
